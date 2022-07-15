5b2be693dff2fa8c56b1ec5c48492e7329b0e11a
Joseph <cj.donato10@gmail.com>
2022-07-15 23:41:14 +0200

    Add & init prisma and basic db operations

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
 const express = require('express')
+const { PrismaClient } = require('@prisma/client') // TODO require vs from
+const cors = require('cors');
+
+const prisma = new PrismaClient()
 const app = express()
-const port = 3000
+const port = 3001
+
+app.use(express.json())
+app.use(cors({ origin: true, credentials: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!!')
+})
+
+app.get('/todo', async (req, res) => {
+  const todos = await prisma.todo.findMany()
+  res.json(todos)
+})
+
+app.post('/todo', async (req, res) => {
+  const { name } = req.body
+
+  const result = await prisma.todo.create({
+    data: { name }
+  })
+
+  res.json(result)
+})
+
+app.put('/todo', async (req, res) => {
+  const { id, name, isDone } = req.body
+
+  const result = await prisma.todo.update({
+    where: { id },
+    data: { name, isDone }
+  })
+
+  res.send(result)
+})
+
+app.delete('/todo', async (req, res) => {
+  const { id } = req.body
+
+  const result = await prisma.todo.delete({
+    where: { id }
+  })
+
+  res.send(result)
 })
 
 app.listen(port, () => {
